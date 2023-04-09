d6c5e3ba5209373cd56320b1aaad195134916c6d
callanhunter <callan57hunter@gmail.com>
2023-04-09 16:53:46 -0700

    seeded the database

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,17 @@ DROP DATABASE IF EXISTS movies_db;
 CREATE DATABASE movies_db;
 
 USE movies_db;
+
+CREATE TABLE movies (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  movie_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE reviews (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  movie_id INT NOT NULL
+  movie_review TEXT NOT NULL,
+  FOREIGN KEY (movies)
+  REFERENCES movies(id)
+  ON DELETE SET NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO movies (movie_name)
+VALUES ("Coco"),
+       ("Click"),
+       ("Dear John");
+
+INSERT INTO reviews (movie_id, review)
+VALUES (1, "It was emotional, colorful, smart and teaches the importance of celebrating family"),
+       (2, "When the sad part came on I cried so much, I couldn't breath"),
+       (3, "It's a heart-warming, but sad, love story"),
+       (2, "Probably the most underrated movie ever"),
+       (3, "I love how you can lose yourself into the characters, their emotions and the romance of it"),
+       (1, "The visuals are stunning and the characters are fantastic!"),
+       
